10283cbe553e277f4d65e38062d33d59b78511b5
changmingMeng <471351371@qq.com>
2017-03-31 22:49:14 +0800

    实现从百度地图api获得地址的经纬度，虽然用一种很丑的方式

--- baiduMap.py
+++ baiduMap.py
@@ -8,7 +8,7 @@ import urllib2
 import  httplib
 import chardet
 
-url = 'http://api.map.baidu.com/geocoder/v2/'
+url = 'http://api.map.baidu.com/geocoder/v2/?address=华景新城&pois=1&city=广州&output=json'
 ak = '&ak=Rl3cuYvuDMBES9TULIixBWAIOi0ES7BN'  # 百度地图api密钥
 # back='&callback=renderReverse&location='
 address = 'address='
@@ -18,21 +18,28 @@ city = '&city=广州'
 citylimit = '&city_limit=true'
 output = '&output=json'
 
-url = url + '?' + address + location + city + output + ak
+url = url + ak
 print url
 
 temp = urllib2.urlopen(url)
 # console.log(data)
-print temp, "strtype:", chardet.detect(temp.read())
-print temp.read()
+# print (temp, "strtype:", chardet.detect(temp.read()))
+# print temp.read()
 #str = '{"status":0,"result":{"location":{"lng":113.36936290434473,"lat":23.139391660850447},"precise":0,"confidence":50,"level":"地产小区"}}'
 #str_json = json.loads(str)
 #print "str_json:", str_json
 #print "lin", str_json["result"]["location"]["lng"]
 
-new_str = temp.read()
-hjson = json.loads(new_str)
-location = hjson["result"]["formatted_address"]  # 省，市，县
-print location
-info = hjson["result"]["sematic_description"]  # 详细描述
-print info
+str = temp.read()
+print str
+file = open('e:\\test.txt','w')
+file.write(str)
+file.close()
+
+file = open('e:\\test.txt', 'r')
+str = file.read()
+print str
+data = json.loads(str)
+lng = data["result"]["location"]["lng"]
+lat = data["result"]["location"]["lat"]
+print "lng=", lng, "lat=", lat
