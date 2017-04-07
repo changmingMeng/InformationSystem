f1db26b8026f0195f97f65a9af34789c504c5ace
changmingMeng <471351371@qq.com>
2017-04-07 09:00:29 +0800

    对应place API和geocding API做了两套接口，place API AK每日使用次数2000，geocoding API每日使用次数6000

--- baiduMap.py
+++ baiduMap.py
@@ -1,45 +1,201 @@
 #! /usr/bin/python
 # -*- coding: utf-8 -*-
 
-import os.path
+import xlrd
+from openpyxl import Workbook
 import csv
 import json
 import urllib2
-import  httplib
+from utils import timer
 import chardet
 
-url = 'http://api.map.baidu.com/geocoder/v2/?address=华景新城&pois=1&city=广州&output=json'
-ak = '&ak=Rl3cuYvuDMBES9TULIixBWAIOi0ES7BN'  # 百度地图api密钥
-# back='&callback=renderReverse&location='
-address = 'address='
-location = '华景新城'
-city = '&city=广州'
 
-citylimit = '&city_limit=true'
-output = '&output=json'
+# url = 'http://api.map.baidu.com/geocoder/v2/?address=华景新城&pois=1&city=广州&output=json'
+# ak = '&ak=Rl3cuYvuDMBES9TULIixBWAIOi0ES7BN'  # 百度地图api密钥
 
-url = url + ak
-print url
+# address = 'address='
+# location = '上社'
+# city = '&city=广州'
+#
+# citylimit = '&city_limit=true'
+# output = '&output=json'
+#
+# url = url + ak
+# print url
+#
+# temp = urllib2.urlopen(url)
+#
+#
+# str = temp.read()
+# print str
+# file = open('e:\\test.txt','w')
+# file.write(str)
+# file.close()
+#
+# file = open('e:\\test.txt', 'r')
+# str = file.read()
+# print str
+# data = json.loads(str)
+# lng = data["result"]["location"]["lng"]
+# lat = data["result"]["location"]["lat"]
+# print "lng=", lng, "lat=", lat
 
-temp = urllib2.urlopen(url)
-# console.log(data)
-# print (temp, "strtype:", chardet.detect(temp.read()))
-# print temp.read()
-#str = '{"status":0,"result":{"location":{"lng":113.36936290434473,"lat":23.139391660850447},"precise":0,"confidence":50,"level":"地产小区"}}'
-#str_json = json.loads(str)
-#print "str_json:", str_json
-#print "lin", str_json["result"]["location"]["lng"]
+url = 'http://api.map.baidu.com/geocoder/v2/'
+placeAPIUrl = 'http://api.map.baidu.com/place/v2/search'
+ak_lst = [
 
-str = temp.read()
-print str
-file = open('e:\\test.txt','w')
-file.write(str)
-file.close()
+          '38ADFrK5VVdptqmai3HnjN78p4RjjQeB',
+          'UYK8YTMm8ly6Sz1aMcnXsXTO9ohni2ex',
+          'YjkhAC8a6XEZqtCN0kSGL8GI1OVLTCly',
+          'Rl3cuYvuDMBES9TULIixBWAIOi0ES7BN',
+          'Hej6HgVATRb3c1brHnoLh9nnfmYiP6YX',
 
-file = open('e:\\test.txt', 'r')
-str = file.read()
-print str
-data = json.loads(str)
-lng = data["result"]["location"]["lng"]
-lat = data["result"]["location"]["lat"]
-print "lng=", lng, "lat=", lat
+          ]
+csv_order_col_number = 0
+csv_address_col_number = 16
+excel_order_col_number = 0
+excel_address_col_number = 0
+#output_col_name = {1:"订单号", 2:"经度", 3:"纬度"}
+output_col_name = {1:"编号", 2:"订单号", 3:"经度", 4:"纬度"}
+class baiduMap(object):
+
+    @staticmethod
+    def getUrl(address, ak=ak_lst[0]):
+        option = "city=广州&output=json&ret_coordtype=gcj02ll&address="#&coordtype=gcj02ll
+        print 'address: ', address
+        return url+"?"+option+address+"&ak="+ak
+
+    @staticmethod
+    def getUrlForPlaceAPI(address, ak=ak_lst[0]):
+        print 'address: ', address.decode("GBK").encode("utf-8")
+        option = "region=广州&scope=1&ret_coordtype=gcj02ll&output=json&query="#ret_coordtype=gcj02ll&
+        return placeAPIUrl+"?"+option+address+"&ak="+ak
+
+    @staticmethod
+    def strWork(str):
+        str = str.replace(" ","").replace("-","").replace('"', '').replace("=", "")#去除空格和-
+        if str.count("区".decode("utf-8").encode("GBK"))>1 or str.count("市".decode("utf-8").encode("GBK"))>2:
+            str = str[22:]
+        return str
+
+    @classmethod
+    @timer
+    def getAxis(cls, address, ak=ak_lst[0], mode="geocoder"):
+        #print 'address: ', address.decode('GBK').encode('utf-8')
+        address = cls.strWork(address)
+        #print 'new address: ', address.decode('GBK').encode('utf-8')
+        #return [0, 0]
+        if mode == "geocoder":
+            url = cls.getUrl(address.decode("GBK").encode("utf-8"), ak)
+        #url = cls.getUrlForPlaceAPI(address, ak)
+            print url
+            temp = urllib2.urlopen(url, timeout=2)
+            str = temp.read()
+            data = json.loads(str)
+
+            lng = data["result"]["location"]["lng"]
+            lat = data["result"]["location"]["lat"]
+        else:
+            url = cls.getUrlForPlaceAPI(address.decode("GBK").encode("utf-8"), ak)
+            print url
+            temp = urllib2.urlopen(url, timeout=2)
+            str = temp.read()
+            data = json.loads(str)
+            lng = data["results"][0]["location"]["lng"]
+            lat = data["results"][0]["location"]["lat"]
+
+        return [lng, lat]
+
+
+def readCSV(filepath):
+    lst = []
+
+    csvReader = csv.reader(file(filepath, 'rb'))
+    for i in xrange(3):
+        csvReader.next()
+    for row in csvReader:
+        lst.append([row[csv_order_col_number], row[csv_address_col_number]])
+    return lst
+
+def readExcel(filepath):
+    lst = []
+    with xlrd.open_workbook(filepath) as workbook:
+        sheet = workbook.sheet_by_index(0)
+        rown = sheet.nrows
+
+        for r in range(rown):
+            lst.append([sheet.cell_value(r, excel_order_col_number),
+                        sheet.cell_value(r, excel_address_col_number)])
+    return lst
+
+def getAddressList(filepath):
+    if filepath.endswith(".csv"):
+        return readCSV(filepath)
+    elif filepath.endswith((".xls", ".xlsx")):
+        return readExcel(filepath)
+
+
+def multiSearch(filepath):
+    addresslist = getAddressList(filepath)
+    #print addresslist
+    #print len(addresslist)
+    axislist = []
+    i = 1
+    print "filelength= ", len(addresslist)
+    for address in addresslist:
+        try:
+            axis = baiduMap.getAxis(address[1], ak_lst[i/50000], "geocoder")
+            print i, axis
+            axislist.append([i,address[0],axis[0],axis[1]])
+        except:
+            print i, address
+            #axislist.append([i,address[0], address[1], "未取到"])
+        finally:
+            i += 1
+            #if
+
+    return axislist
+
+def writeExcel(axislist, filepath):
+    wb = Workbook()
+    ws = wb.active
+
+    ws.cell(row=1, column=1, value=output_col_name[1])
+    ws.cell(row=1, column=2, value=output_col_name[2])
+    ws.cell(row=1, column=3, value=output_col_name[3])
+    ws.cell(row=1, column=4, value=output_col_name[4])
+
+    r = 1
+    for axis in axislist:
+        print r
+        r += 1
+        ws.cell(row=r, column=1, value=axis[0])
+        ws.cell(row=r, column=2, value=axis[1])
+        ws.cell(row=r, column=3, value=axis[2])
+        ws.cell(row=r, column=4, value=axis[3])
+
+    wb.save(filepath)
+
+
+def test(inputfilepath, outputfilepath):
+    writeExcel(multiSearch(inputfilepath), outputfilepath)
+
+
+if __name__ == "__main__":
+    #print multiSearch(r"F:\视频组\投诉\3-30\address.csv".decode("utf-8").encode("GBK"))
+    #print baiduMap.getAxis("华南理工大学广州学院",ak_lst[0])
+    #print getAddressList(r"F:\视频组\投诉\3-30\address.xlsx".decode("utf-8").encode("GBK"))
+    test(r"F:\视频组\地址经纬度\bilibili广州.csv".decode("utf-8").encode("GBK"),
+         r"F:\视频组\地址经纬度\bilibili广州_result.xlsx".decode("utf-8").encode("GBK"))
+    #[113.36763702320629, 23.14023770937979]
+    #[113.36763702320629, 23.14023770937979]
+
+    #[113.17212192764, 23.435253319248]
+    #[113.178584, 23.441531]
+    #print getAddressList(r"F:\视频组\投诉\3-30\address.csv".decode("utf-8").encode("GBK"))
+    #print "广东广州市黄埔区  广东省广州  市花都区迎宾大道名高思埠大厦".replace(" ","")
+    #http://api.map.baidu.com/place/v2/search?query=%E4%B8%8A%E7%A4%BE&scope=1&region=%E5%B9%BF%E5%B7%9E&output=json&ret_coordtype=gcj02ll&ak=UYK8YTMm8ly6Sz1aMcnXsXTO9ohni2ex
+    # http://api.map.baidu.com/place/v2/search?query=广东广州市黄埔区广东省广州市花都区迎宾大道名高思埠大厦&scope=1&region=%E5%B9%BF%E5%B7%9E&output=json&ret_coordtype=gcj02ll&ak=UYK8YTMm8ly6Sz1aMcnXsXTO9ohni2ex
+    # http://api.map.baidu.com/place/v2/search?query=广东广州市黄埔区广东省广州市花都区迎宾大道名高思埠大厦&region=广州&scope=1&output=json&ret_coordtype=gcj02ll&ak=UYK8YTMm8ly6Sz1aMcnXsXTO9ohni2ex
+    # address = "广东广州市黄埔区广东省 广州市 越秀区    一德路498号一楼a5档"
+    # print baiduMap.strWork(address)
